e27f868c45042dca7c0cfcf463f954b4c31cf9b4
Neamul01 <neamulhaque2002@gmail.com>
2024-02-26 22:12:39 +0600

    add track stydy file

---
+++ app/tabs/study/TrackStudy.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const TrackStudy = () => {
+  return (
+    <View
+      className={`flex-1 items-center justify-between bg-gray-100 px-5 py-4`}
+    >
+      <Text>TrackStudy</Text>
+    </View>
+  );
+};
+
+export default TrackStudy;
